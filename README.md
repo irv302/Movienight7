960340e7fca51c526010cb5125c887eb46015f77
Raquel Olaya <raquelolaya@Raquels-MacBook-Air.local>
2022-06-02 02:10:46 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# m
+# movienight7
--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
+app.get('/', (req, res) => {
+    res.render('movies/new.ejs');
+});
+
 app.listen(3000, () => {
     console.log('listening ')
 });
