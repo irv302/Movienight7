71cf79920525689b090acd4fc51448d6bbeeb84c
Raquel Olaya <raquelolaya@Raquels-MacBook-Air.local>
2022-06-02 01:54:38 -0500

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const moviesController = require('./controllers/movies');
+
+
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
