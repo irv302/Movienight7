81a016f84abdc90c8ea99c7d71ea3e052b782c50
Raquel Olaya <raquelolaya@Raquels-MacBook-Air.local>
2022-06-03 00:05:06 -0500

    connecting to mongo

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
+require('dotenv').config();
 const express = require('express');
 const moviesController = require('./controllers/movies');
 const app = express();
-
-
+const mongoose = require('mongoose');
 const PORT = 3000
 
-app.use('movies', moviesController);
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParaser: true,
+    useUnifiedTopology: true,
+
+});
+
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err.message + ' No mangos ?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+app.use('/movies', require('./controllers/movies'));
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
 });
-
-
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
